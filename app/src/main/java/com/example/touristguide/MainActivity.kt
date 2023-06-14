c4c17dfd7c83dd72cb11e978353717da0ed606e4
karan saggoo <saggookaran@gmail.com>
2023-06-14 15:50:13 -0400

    TG-11 Creating Nav graph and setting up in main activity

--- app/src/main/java/com/example/touristguide/MainActivity.kt
+++ app/src/main/java/com/example/touristguide/MainActivity.kt
@@ -1,11 +1,65 @@
 package com.example.touristguide
 
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.touristguide.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
+    private lateinit var prefs: SharedPreferences
+    var name = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        prefs=applicationContext.getSharedPreferences(packageName, AppCompatActivity.MODE_PRIVATE)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        this.navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.log_out->{
+                deletePreferences()
+                var intent = Intent(this,SignInActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> {
+                return super.onOptionsItemSelected(item)
+            }
+        }
+    }
+
+    fun deletePreferences() {
+        with(prefs.edit()) {
+            clear()
+            apply()
+
+        }
+
     }
 }
