33a442787056b8dff4b5ac4ddc33410c0a9bd6a9
harsh <harshdeep4544@gmail.com>
2023-06-14 20:00:41 -0400

    TG-7 configuring Image buttons to respond to clicks

--- app/src/main/java/com/example/touristguide/HomeScreen.kt
+++ app/src/main/java/com/example/touristguide/HomeScreen.kt
@@ -1,7 +1,9 @@
 package com.example.touristguide
 
+import android.app.AlertDialog
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.text.Html
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -42,10 +44,62 @@ class HomeScreen : Fragment() {
         name = prefs.getString("USER_NAME","").toString()
         binding.name.setText("${name}")
         binding.btn1.setOnClickListener {
-           // val action = HomeScreenDirections.actionHomeScreenToList2 ()
-           // findNavController().navigate(action)
+            val category = "restaurant"
+            val action = HomeScreenDirections.actionHomeScreenToList2(category)
+            findNavController().navigate(action)
+        }
+
+        binding.searchBtn.setOnClickListener {
+            var category = binding.querySearch.text.toString()
+            if (category != "accounting" && category != "aquarium" && category != "bar" &&
+                category != "bank" && category != "museum" && category != "parking" &&
+                category != "bus station"
+            ) {
+
+                AlertDialog.Builder(requireContext())
+                    .setTitle("Invalid Category")
+                    .setMessage(
+                        Html.fromHtml(
+                        "<b>"+ "Category should be either of the following:<br>"+"</b>"+"accounting<br>aquarium<br>bar<br>bank<br>museum<br>parking<br>bus station",
+                            Html.FROM_HTML_MODE_LEGACY
+                    ))
+                    .setNegativeButton("Ok", null).show()
+            }
+            else{
+                if(category=="bus station")
+                    category="bus_station"
+                val action = HomeScreenDirections.actionHomeScreenToList2(category)
+                findNavController().navigate(action)
+            }
+        }
+        binding.btn2.setOnClickListener {
+            val category = "cafe"
+            val action = HomeScreenDirections.actionHomeScreenToList2(category)
+            findNavController().navigate(action)
+        }
+        binding.btn3.setOnClickListener {
+            val category = "shop"
+            val action = HomeScreenDirections.actionHomeScreenToList2(category)
+            findNavController().navigate(action)
+        }
+        binding.btn4.setOnClickListener {
+            val category = "library"
+            val action = HomeScreenDirections.actionHomeScreenToList2(category)
+            findNavController().navigate(action)
+        }
+        binding.btn5.setOnClickListener {
+            val category = "hospital"
+            val action = HomeScreenDirections.actionHomeScreenToList2(category)
+            findNavController().navigate(action)
+        }
+        binding.btn6.setOnClickListener {
+            val category = "theater"
+            val action = HomeScreenDirections.actionHomeScreenToList2(category)
+            findNavController().navigate(action)
         }
 
     }
 
+
 }
+
