d344c3e1c71012904690c4445302623c982f1ed2
harsh <harshdeep4544@gmail.com>
2023-06-14 20:48:58 -0400

    TG-15 Configuring List fragment to show the list fetched from api

--- app/src/main/java/com/example/touristguide/List.kt
+++ app/src/main/java/com/example/touristguide/List.kt
@@ -1,59 +1,101 @@
 package com.example.touristguide
 
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.project_g08.api.IAPIResponse
+import com.example.project_g08.api.RetrofitInstance
+import com.example.touristguide.adapter.PlaceAdapter
+import com.example.touristguide.adapter.onPlaceClickListener
+import com.example.touristguide.api.IAPIResponse2
+import com.example.touristguide.api.LocationHelper
+import com.example.touristguide.databinding.FragmentHomeScreenBinding
+import com.example.touristguide.databinding.FragmentListBinding
+import com.example.touristguide.model.Detail
+import com.example.touristguide.model.Place
+import com.example.touristguide.model.PlaceDetail
+import com.google.firebase.firestore.local.LruGarbageCollector
+import kotlinx.coroutines.launch
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class List : Fragment(),onPlaceClickListener {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [List.newInstance] factory method to
- * create an instance of this fragment.
- */
-class List : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding : FragmentListBinding? = null
+    private lateinit var locationHelper: LocationHelper
+    private var result:MutableLiveData<Location>? = null
+    private val binding get() = _binding!!
+    private  val args:ListArgs by navArgs()
+    private lateinit var placeListFromAPI: Place
+    private lateinit var placeDetailFromAPI:PlaceDetail
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        this.locationHelper = LocationHelper.instance
+        result = locationHelper.getLastLocation(requireContext())
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment List.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            List().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        var PlaceList: ArrayList<com.example.touristguide.model.Result> = ArrayList()
+
+        var adapter: PlaceAdapter = PlaceAdapter(requireContext(), PlaceList, this)
+        binding.rvView.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvView.adapter = adapter
+
+        var api: IAPIResponse = RetrofitInstance.retrofitService
+        var lat = result!!.value!!.latitude
+        var log = result!!.value!!.longitude
+        var location:String = "${lat},${log}"
+
+        //Retrieve world news from API
+        viewLifecycleOwner.lifecycleScope.launch {
+
+            when(args.category){
+
+                "restaurant"-> placeListFromAPI = api.getRestaurant(location)
+                "cafe"->placeListFromAPI=api.getCafe(location)
+                "hospital"->placeListFromAPI=api.getHospital(location)
+                "library"->placeListFromAPI=api.getLibrary(location)
+                "shop"->placeListFromAPI=api.getShopping(location)
+                "theater"->placeListFromAPI=api.getTheatre(location)
+
+
             }
+
+            PlaceList.clear()
+            PlaceList.addAll(placeListFromAPI.results)
+            adapter.notifyDataSetChanged()
+            Log.d("world","${PlaceList.size}")
+            Log.d("world", "${placeListFromAPI.results}")
+        }
     }
+
+    override fun onItemClickListener(place_id:String , place:com.example.touristguide.model.Result) {
+
+
+
+    }
+
+
 }
