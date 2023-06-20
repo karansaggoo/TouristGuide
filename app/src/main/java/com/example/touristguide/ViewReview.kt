c68c35d6153ed7db2c8235d68fdcd7c067f72e1a
harsh <harshdeep4544@gmail.com>
2023-06-19 22:28:01 -0400

    TG-18 adding composable functions in view review fragment

--- app/src/main/java/com/example/touristguide/ViewReview.kt
+++ app/src/main/java/com/example/touristguide/ViewReview.kt
@@ -5,9 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.touristguide.databinding.FragmentViewReviewBinding
+import com.example.touristguide.databinding.FragmentWishListBinding
+import com.example.touristguide.model.Review
+
 
 
 class ViewReview : Fragment() {
+    private var _binding: FragmentViewReviewBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,9 +40,57 @@ class ViewReview : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentViewReviewBinding.inflate(inflater,container,false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_review, container, false)
+        binding.composeView.apply {
+            // Dispose of the Composition when the view's LifecycleOwner
+            // is destroyed
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                // In Compose world
+                MaterialTheme {
+
+
+                }
+            }
+        }
+        val view = binding.root
+
+        return view
+
     }
 
 
+
+
 }
+
+
+@Composable
+fun ReviewList(reviews: ArrayList<Review>) {
+    LazyColumn {
+        items(reviews){review->
+            ReviewCard(rvw = review)
+            
+        }
+    }
+}
+
+
+@Composable
+fun ReviewCard(rvw: Review) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+
+        Column {
+            Text(
+                text = rvw.author_name,
+                color = Color(0xFFFFa500),
+                style = TextStyle(fontSize = 25.sp)
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = rvw.text)
+        }
+    }
+}
+
