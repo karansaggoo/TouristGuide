e2b6e47c1834019dc5154c993c7a09d9d3b2a510
harsh <harshdeep4544@gmail.com>
2023-07-02 23:09:39 -0400

    TG-43 storing photos to firebase storage

--- app/src/main/java/com/example/touristguide/GuideProfile.kt
+++ app/src/main/java/com/example/touristguide/GuideProfile.kt
@@ -15,6 +15,8 @@ import androidx.fragment.app.Fragment
 import com.example.touristguide.databinding.FragmentGuideProfileBinding
 import com.example.touristguide.model.Guide
 import com.example.touristguide.model.GuideRepository
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 
 class GuideProfile : Fragment() {
@@ -22,7 +24,8 @@ class GuideProfile : Fragment() {
     private val binding get() = _binding!!
     lateinit var guideRepository: GuideRepository
     private final var RC_PHOTO_PICKER =2
-    private var selectedImageUri:Uri? =null
+    private var selectedImageUri:String?=null
+    private lateinit var photoStorageRefeference:StorageReference
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,6 +38,8 @@ class GuideProfile : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentGuideProfileBinding.inflate(inflater, container, false)
+        photoStorageRefeference  = FirebaseStorage.getInstance().getReference().child("profile_photos")
+
         guideRepository = GuideRepository(requireContext())
         val view = binding.root
         return view
@@ -46,33 +51,29 @@ class GuideProfile : Fragment() {
 
                 pickMedia.launch(PickVisualMediaRequest(PickVisualMedia.ImageOnly))
 
-//            var intent = Intent(Intent.ACTION_GET_CONTENT)
-//            intent.setType("images/*")
-//           // intent.putExtra(Intent.EXTRA_LOCAL_ONLY,true)
-//            startActivityForResult(Intent.createChooser(intent,"complete action using"),RC_PHOTO_PICKER)
         }
 
         binding.profileUpdate.setOnClickListener {
-            guideRepository.addUserToDB(Guide("harsh","Harsh",123456,"toronto","YYZ",selectedImageUri.toString()))
+            guideRepository.addUserToDB(Guide(email = "harsh", name = "fcd", tel = 123456, desc = "vfdctvf", loc = "toronto", imageUri = selectedImageUri!!))
         }
 
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode==RC_PHOTO_PICKER && resultCode== RESULT_OK){
-             selectedImageUri = data!!.data
-            if (selectedImageUri != null) {
-                binding.profilePic.setImageURI(selectedImageUri)
-//                var photoRef = mChatPhotoStorageRefeference.child(selectedImageUri.lastPathSegment!!)
-//                photoRef.putFile(selectedImageUri).addOnSuccessListener { taskSnapshot ->
-//                    var downloadUrl = taskSnapshot.uploadSessionUri
-//                    var friendlyMessage = FriendlyMessage(null,mUsername,downloadUrl.toString())
-//                    mMessageDatabaseReference.push().setValue(friendlyMessage)
-//
-            }
-        }
-    }
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//        if(requestCode==RC_PHOTO_PICKER && resultCode== RESULT_OK){
+//             selectedImageUri = data!!.data
+//            if (selectedImageUri != null) {
+//                binding.profilePic.setImageURI(selectedImageUri)
+////                var photoRef = mChatPhotoStorageRefeference.child(selectedImageUri.lastPathSegment!!)
+////                photoRef.putFile(selectedImageUri).addOnSuccessListener { taskSnapshot ->
+////                    var downloadUrl = taskSnapshot.uploadSessionUri
+////                    var friendlyMessage = FriendlyMessage(null,mUsername,downloadUrl.toString())
+////                    mMessageDatabaseReference.push().setValue(friendlyMessage)
+////
+//            }
+//        }
+//    }
     var pickMedia = registerForActivityResult(
         PickVisualMedia()
     ) { uri: Uri? ->
@@ -80,8 +81,13 @@ class GuideProfile : Fragment() {
         // photo picker.
         if (uri != null) {
             binding.profilePic.setImageURI(uri)
-            Log.d("PhotoPicker", "Selected URI: $uri")
-        } else {
+            var photoRef = photoStorageRefeference.child(uri.lastPathSegment!!)
+            photoRef.putFile(uri).addOnSuccessListener { taskSnapshot ->
+                var downloadUrl = taskSnapshot.uploadSessionUri
+                selectedImageUri = downloadUrl.toString()
+                Log.d("PhotoPicker", "Selected URI: $uri")
+            }
+        }else {
             Log.d("PhotoPicker", "No media selected")
         }
     }
